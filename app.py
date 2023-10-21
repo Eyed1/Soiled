bad02666d98df6b68cd4843bbae66a9d07e9e2d8
Jeff <jeffreychen51@gmail.com>
2023-10-21 10:59:01 -0400

    personal

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from flask import send_from_directory
+import sys
+import os
+sys.path.append('./backend')
+import predictor
+
+app = Flask(__name__, static_url_path='', static_folder='form')
+CORS(app)
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+@app.route("/form_submit", methods=['POST'], strict_slashes=False)
+def get_form():
+    nitrogen = request.json()['nitrogen']
+    phosphorus = request.json()['phosphorus']
+    potassium = request.json()['potassium']
+    pH = request.json()['pH']
+    temperature = request.json()['temperature']
+    rainfall = request.json()['rainfall']
+    return predictor.get_prediction(nitrogen, phosphorus, potassium, pH, temperature, rainfall)
+
+
+
+
+"""
+app.route('/data', methods=['GET'])
+def get_data():
+    # Your code to get data from the React JS file goes here
+    return jsonify(data)
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- backend/predictor.py
+++ backend/predictor.py
@@ -4,13 +4,14 @@ import datetime
 from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
-
+import os
 
 def get_prediction(date, N, P, K, pH, rain, temp):
     good_prods = hc.get_valid(date, temp, pH)
     if (len(good_prods) == 0):
         return "None"
-    tot_data = pd.read_csv("tot_data.csv")
+    cwd = os.getcwd()
+    tot_data = pd.read_csv(f"{cwd}/backend/tot_data.csv")
     use_data = tot_data[tot_data["product"].isin(good_prods)]
 
     model = SVC()
