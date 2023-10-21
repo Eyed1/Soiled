72ffc8dc3a5ea608cf3990a9b8dcc10a821aebc1
Jack Chen <cjack8855@gmail.com>
2023-10-21 11:32:34 -0400

    asfd

--- soiled/src/components/DirtForm.tsx
+++ soiled/src/components/DirtForm.tsx
@@ -2,20 +2,48 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 let renderCount = 0;
-export const DirtForm = () => {
-  const form = useForm();
-  const { register, control } = form;
 
+type FormValues = {
+  nitrogen: string;
+  phosphorous: string;
+  potassium: string;
+  date: string;
+  pH: string;
+  temperature: string;
+  rainfall: string;
+};
+export const DirtForm = () => {
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
   renderCount++;
   return (
     <div>
       <h1>Dirt Form ({renderCount / 2})</h1>
-      <form>
-        <label htmlFor="location">Location</label>
-        <input type="text" id="location" {...register("location")} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="nitrogen">Nitrogen Levels</label>
+        <input type="text" id="nitrogen" {...register("nitrogen")} />
+
+        <label htmlFor="phosphorous">Phosphorous Levels</label>
+        <input type="text" id="phosphorous" {...register("phosphorous")} />
+
+        <label htmlFor="potassium">Potassium Levels</label>
+        <input type="text" id="potassium" {...register("potassium")} />
 
         <label htmlFor="date">Date</label>
-        <input type="text" id="date" {...register("date")} />
+        <input type="date" id="date" {...register("date")} />
+
+        <label htmlFor="pH">pH levels</label>
+        <input type="text" id="pH" {...register("pH")} />
+
+        <label htmlFor="temperature">Temperature</label>
+        <input type="text" id="temperature" {...register("temperature")} />
+
+        <label htmlFor="rainfall">Average Rainfall</label>
+        <input type="text" id="rainfall" {...register("rainfall")} />
 
         <button>Submit</button>
       </form>
